cf603729e3841b22e18bf905b55be63a62276b00
mauroef <cypress90@gmail.com>
2015-06-27 21:03:41 -0300

    Update funcionSQL.php

--- funcionSQL.php
+++ funcionSQL.php
@@ -1,5 +1,5 @@
 <?php 
-	function abriendoConexionSQL($host,$usuario,$bd,$pass=''){
+	function abriendoConexionSQL($host,$usuario,$bd,$pass){
 		$conexion=mysql_connect($host,$usuario,$pass);
 		if(!mysql_select_db($bd)) return false;
 		return $conexion;
