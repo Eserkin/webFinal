1e89e8fd7f7387e7110e947016b0342500df35b8
mauroef <cypress90@gmail.com>
2015-07-08 01:57:08 -0300

    Update codigoSistemaQr.php

--- codigoSistemaQr.php
+++ codigoSistemaQr.php
@@ -5,11 +5,9 @@
   include_once("funcionSQL.php");
 
   $link=abriendoConexionSQL();
-  //provisorio:
   $consulta=consultaDatos("SELECT codigo_desbloqueo FROM sistema WHERE cliente_id='$id';");
   $line=mysql_fetch_assoc($consulta);
   $codigo='El codigo de su sistema es: '. mysql_result($consulta,'0');
-  //solo accede a una fila especifica (1)
   QRcode::png($codigo);
   desconectarSQL($link);
  ?>
